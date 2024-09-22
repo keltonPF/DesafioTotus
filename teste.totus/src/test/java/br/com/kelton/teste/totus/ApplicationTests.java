6d1628733cf0acb839e0275483afba5fc72ea934
KeltonEPF <arilton_kelton@hotmail.com>
2024-09-22 12:50:15 -0300

    Iniciando

---
+++ teste.totus/src/test/java/br/com/kelton/teste/totus/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kelton.teste.totus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
